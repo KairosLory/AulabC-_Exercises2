b7891fe65bd7e2a760e32a2b54ff84264230b712
Lorenzo Pedoni <lorenzopedoni53@gmail.com>
2026-05-27 15:42:11 +0200

    Secondo commit Esercizio1 sul Polimorfismo.

--- Esercizio1_Polimorfismo/Person.cs
+++ Esercizio1_Polimorfismo/Person.cs
@@ -5,7 +5,7 @@ public abstract class Person
 	public string Name { get; init; }
 	public string Surname { get; init; }
 	public string CountryOfBirth{ get; init; }
-	public DateTime DateOfBithday { get; init; }
+	public DateOnly DateOfBithday { get; init; }
 
 	public abstract void DisplayPersonInfo();
 }
--- Esercizio1_Polimorfismo/Portal.cs
+++ Esercizio1_Polimorfismo/Portal.cs
@@ -5,21 +5,22 @@ public static class Portal
 	public static Type StartPortal()
 	{
         Console.WriteLine("Benevenuto nel portale digitale dell'Università di Lorenzo!");
-        Console.WriteLine("Con che tipo di ruolo vuoi loggarti? (studente/professore)");
+        Console.Write("Con che tipo di ruolo vuoi loggarti? (studente/professore)");
 
         string inputRoleString = (Console.ReadLine() ?? "Spiacente, ma è una null string").Trim().ToLower();
 
         while(inputRoleString != "studente" &&  inputRoleString != "professore")
         {
             Console.WriteLine("Mi dispiace ma hai inserito un ruolo non valido o non presente!");
-            Console.WriteLine("Reinserisci il ruolo: ");
+            Console.Write("Reinserisci il ruolo: ");
             inputRoleString = (Console.ReadLine() ?? "Spiacente, ma è una null string").Trim().ToLower();
         }
 
         Type typeReturnStartPortal = inputRoleString switch
         {
             "studente" => typeof(Student),
-            "professore" => typeof(Professor)
+            "professore" => typeof(Professor),
+            _ => throw new Exception("Qualcoa è andato storto...")
         };
 
         return typeReturnStartPortal;
@@ -43,20 +44,20 @@ public static class Portal
         Console.WriteLine("Benvenuto studente!");
 
         Console.WriteLine("Inserisci ora i tuoi dati!");
-        Console.WriteLine("Nome: ");
-        string studentName = Console.ReadLine();
-        Console.WriteLine("Cognome: ");
-        string studentSurname = Console.ReadLine();
-        Console.WriteLine("Città di nascita: ");
-        string studentCountryOfBirth = Console.ReadLine();
-        Console.WriteLine("Data di nascita: ");
-        string studentDateOfBirthdayString = Console.ReadLine();
-        DateTime studentDateOfBirthday;
+        Console.Write("Nome: ");
+        string studentName = (Console.ReadLine() ?? "");
+        Console.Write("Cognome: ");
+        string studentSurname = (Console.ReadLine() ?? "");
+        Console.Write("Città di nascita: ");
+        string studentCountryOfBirth = (Console.ReadLine() ?? "");
+        Console.Write("Data di nascita: ");
+        string studentDateOfBirthdayString = (Console.ReadLine() ?? "");
+        DateOnly studentDateOfBirthday;
 
-        while (!DateTime.TryParse(studentDateOfBirthdayString, out studentDateOfBirthday))
+        while (!DateOnly.TryParse(studentDateOfBirthdayString, out studentDateOfBirthday))
         {
-            Console.WriteLine("Data non valida, prova a reinserirla: ");
-            studentDateOfBirthdayString = Console.ReadLine();
+            Console.Write("Data non valida, prova a reinserirla: ");
+            studentDateOfBirthdayString = (Console.ReadLine() ?? "");
         }
 
         Student student = new Student()
@@ -72,7 +73,34 @@ public static class Portal
     }
     public static void ShowMenuProfessor()
     {
+        Console.WriteLine("Benvenuto professore!");
 
+        Console.WriteLine("Inserisci ora i tuoi dati!");
+        Console.Write("Nome: ");
+        string professorName = (Console.ReadLine() ?? "");
+        Console.Write("Cognome: ");
+        string professorSurname = (Console.ReadLine() ?? "");
+        Console.Write("Città di nascita: ");
+        string professorCountryOfBirth = (Console.ReadLine() ?? "");
+        Console.Write("Data di nascita: ");
+        string professorDateOfBithdayString = (Console.ReadLine() ?? "");
+        DateOnly professorDateOfBirthday;
+
+        while (!DateOnly.TryParse(professorDateOfBithdayString, out professorDateOfBirthday))
+        {
+            Console.Write("Data non valida, prova a reinserirla: ");
+            professorDateOfBithdayString = (Console.ReadLine() ?? "");
+        }
+
+        Professor professor  = new Professor()
+        {
+            Name = professorName,
+            Surname = professorSurname,
+            CountryOfBirth = professorCountryOfBirth,
+            DateOfBithday = professorDateOfBirthday
+        };
+
+        PortalDisplayPersonalInfo(professor);
     }
 
     public static void PortalDisplayPersonalInfo(Person person)
